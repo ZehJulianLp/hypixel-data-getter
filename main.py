6fc77118c4e0302266fe8d1a966442863e28b407
ZehJulianLp <79467458+ZehJulianLp@users.noreply.github.com>
2022-01-20 15:40:45 +0100

    Update - Output file added
    data.json is the output file.

--- main.py
+++ main.py
@@ -10,10 +10,13 @@ while True:
             mojanduuid = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{uuid}").json()
             print("")
             uuid1 = mojanduuid['id']
-            apikey = "API Key from Hypixel. Put it in here from hypixel. You get one with writing /api new on hypixel in Minecraft."
+            apikey = "Put Hypixel API-Key into here"
             r = requests.get(f"https://api.hypixel.net/player?key={apikey}&uuid={uuid1}")
             print("")
             jsonr = r.json()
-            print(json.dumps(jsonr, indent=4, sort_keys=True))
+            out = json.dumps(jsonr, indent=4, sort_keys=True)
+            print(out)
+            with open("data.json", "w") as file:
+                file.write(out)
         except:
             print("Error. Please enter a valid name, UUID or exit.")
